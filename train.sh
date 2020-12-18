6002e984f748a8455a0e1304e5f9b032cc439943
JimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2020-12-18 15:19:29 +0800

    exp: lr1e-2_lrchedulr

--- train.sh
+++ train.sh
@@ -1,21 +1,24 @@
-GPUS='0,1,2,3';
-BATCH_SIZE_PER_GPU=8;
-MAX_EPOCHS=300;
+GPUS='0,1';
+BATCH_SIZE_PER_GPU=16;
+MAX_EPOCHS=200;
 LATENT_SIZE=1024;
-TRAIN_DATA=28000;
-VAL_DATA=1000;
+TRAIN_DATA=27000;
+VAL_DATA=2000;
 TEST_DATA=1000;
-NORM_LAYER_TYPE='spectral_norm';
-LOSSES='ssim,lpips'
+NORM_LAYER_TYPE='batch_norm';
+LOSSES='ssim,lpips';
 LEARNING_RATE='0.01';
+LR_SCHEDULER='MultiStepLR';
 NUM_SAMPLE=16;
-LOG_NAME='test';
-VERSION='1215-MDSlatent-tri-neq-1';
+LOG_NAME='test'; # lpips-tri-neq
+VERSION='lr1e-2_lrchedulr'; # mse-a1-fr1-after1-2
 LOG_SAMPLE_EVERY=2;
 ROOT_DIR='~/data/FFHQ';
 LATENT_PATH='MDS_feat_30000.npy';
 TARGET_DIR='images256x256';
 N_WORKERS=8;
+LOG_EVERY_N_STEPS=50;
+FLUSH_LOGS_EVERY_N_STEPS=500;
  \
 python $1 \
 --root_dir ${ROOT_DIR} \
@@ -30,15 +33,16 @@ python $1 \
 --val_size ${VAL_DATA} \
 --test_size ${TEST_DATA} \
 --norm_type ${NORM_LAYER_TYPE} \
---losses %{LOSSES} \
+--losses ${LOSSES} \
 --lr ${LEARNING_RATE} \
+--lr_scheduler ${LR_SCHEDULER} \
 --num_sample ${NUM_SAMPLE} \
 --log_name ${LOG_NAME} \
 --version ${VERSION} \
 --log_sample_every ${LOG_SAMPLE_EVERY} \
---log_every_n_steps 50 \
---flush_logs_every_n_steps 500 
+--log_every_n_steps ${LOG_EVERY_N_STEPS} \
+--flush_logs_every_n_steps ${FLUSH_LOGS_EVERY_N_STEPS} \
+--sync_batchnorm
 
-# --sync_batchnorm
 # --resume_from_checkpoint
 
