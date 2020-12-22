d4a32ba2dd1b1b5836e3171a3f93c51552f3551d
JimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2020-12-22 23:56:18 +0800

    add norm info

--- models/cnn_decoder.py
+++ models/cnn_decoder.py
@@ -13,6 +13,7 @@ class CNNDecoder(nn.Module):
         self.linear1 = nn.Linear(latent_dim, 4096)
         self.linear2 = nn.Linear(4096, 8*8*256)
         self.act = nn.LeakyReLU(0.2)
+        print(f"using norm: {norm_type}")
 
         for i in range(len(n_ch) - 1):
             conv_blocks.append(self.act)
@@ -21,7 +22,7 @@ class CNNDecoder(nn.Module):
             else:
                 conv = nn.ConvTranspose2d(n_ch[i],n_ch[i+1], kernel_size=4, stride=2, padding=1)
                 
-            if norm_type == 'spectral_norm' and i in [1,2,3,4,5]:
+            if norm_type == 'spectral_norm' and i in [0,1,2,3,4,5]:
                 conv_sn = nn.utils.spectral_norm(conv)
                 conv_blocks.append(conv_sn)
             elif norm_type == 'batch_norm' and i in [4,5]:
