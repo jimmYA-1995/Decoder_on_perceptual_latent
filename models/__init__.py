3a770d732033563d438bbce6e06367aa8afc8bfc
JimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2021-01-25 13:18:24 +0800

    refactor, exp: no interpolated latents

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
-from argparse import ArgumentParser
-from typing import List
 import random
+from statistics import mean
+from argparse import ArgumentParser
+
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 from pytorch_lightning import LightningModule
@@ -14,20 +16,34 @@ from .models import Generator, Discriminator
 from losses import nonsaturating_loss, path_regularize, logistic_loss, d_r1_loss
 
 
-
 def requires_grad(model, flag=True):
     for p in model.parameters():
         p.requires_grad = flag
 
+def latent_interpolation(latent, method='linear'):
+    b, nz = latent.shape
+    
+    if method == 'linear':
+        latent_l, latent_r = latent.view(2, b//2, nz)
+        alpha = torch.rand((b//2,1)).type_as(latent)
+        interpolated_latents = \
+            latent_l * alpha + latent_r * (torch.ones_like(alpha) - alpha)
+    else:
+        raise NotImplementedError("Only linear interpolation supported now")
+    
+    return interpolated_latents
+    
 class LitSystem(LightningModule):
     def __init__(self,
-                 log_sample_every,
                  samples,
+                 log_sample_every: int,
+                 interpolation: str = None,
                  losses: str = 'G:GAN,ppl;D:GAN,r1',
                  lr: float = 0.2,
                  batch_size: int = 32,
                  latent_dim: int = 512,
-                 train_size: int = 3200
+                 train_size: int = 3200,
+                 val_size:   int = 3200
         ):
         
         super(LitSystem, self).__init__()
@@ -39,6 +55,7 @@ class LitSystem(LightningModule):
         self.save_hyperparameters("train_size", "losses", "lr", "batch_size", "latent_dim")
         
         self.latent_dim = latent_dim
+        self.interpolation = interpolation
         self.mixing_prob = 0.9
         self.g_reg_every = 4
         self.d_reg_every = 16
@@ -52,31 +69,32 @@ class LitSystem(LightningModule):
         
         # loss
         self.ce_loss = torch.nn.CrossEntropyLoss()
-        # self.ssim_loss = SSIM()
+        self.mse_loss = nn.MSELoss()
+        self.ssim_loss = SSIM()
         # https://github.com/S-aiueo32/lpips-pytorch
         # not using official repo. because we can't use sub-DataParallel model in pytorh-lightning
-        # self.percept = LPIPS(
-        #     net_type='alex',
-        #     version='0.1'
-        # )
-        # for param in self.percept.parameters():
-        #     param.requires_grad = False
+        self.percept = LPIPS(
+            net_type='alex',
+            version='0.1'
+        )
+        requires_grad(self.percept, False)
         
         # metric & log
         self.log_sample_every = log_sample_every
-        
-        self.mean_path_length = 0
+        self.best_mse = 4.
+        self.best_ssim = 0.
+        self.best_lpips = float("inf")
         self.register_buffer("path_lengths", torch.tensor(0.0))
+        self.register_buffer("mean_path_length", torch.tensor(0.0))
         
         # sample
-        n_sample = samples['train']['latents'].shape[0]
-        
-        self.register_buffer("sample_z", torch.randn(n_sample, latent_dim))
+        self.register_buffer("smpl_indices_train", samples['train']['indices'])
         self.register_buffer("smpl_latent_train", samples['train']['latents'])
         self.register_buffer("smpl_target_train", samples['train']['targets'])
         self.register_buffer("smpl_latent_test", samples['test']['latents'])
         self.register_buffer("smpl_target_test", samples['test']['targets'])
         self.n_sample = self.smpl_latent_train.shape[0]
+        self.register_buffer("sample_z", torch.randn(self.n_sample, latent_dim))
     
     @staticmethod
     def add_model_specific_args(parent_parser):
@@ -86,26 +104,26 @@ class LitSystem(LightningModule):
         parser.add_argument('--log_sample_every', type=int, default=10)
         
         return parser
-        
+
     def forward(self, latent, skip_mapping=False):
         # inference
         return self.g([latent], skip_mapping=skip_mapping)
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         weighted_path_loss_val, r1_loss_val = None, None
-        latent, target_img = batch
-        _, nz = latent.shape
-        b, c, h, w = target_img.shape
-        target_img = target_img[:b//2, ...] ### 
-        
-        # interpolated latents
-        latent_l, latent_r = latent.view(2, b//2, nz)
-        alpha = torch.rand((b//2,1)).type_as(latent)
-        interpolated_latents = \
-            latent_l * alpha + latent_r * (torch.ones_like(alpha) - alpha)
-
-        use_reg = False #  if self.current_epoch > 3 else False
+        use_reg = True #if self.current_epoch > 0 else False
         (opt_gm, opt_gs, opt_d) = self.optimizers()
+        _, latent, target_img = batch
+    
+        interpolated_latents = None
+        if self.interpolation:
+            interpolated_latents = latent_interpolation(
+                latent,
+                method=self.interpolation
+            )
+            b = interpolated_latents.shape[0]
+            latent = latent[:b//2, ...]
+            target_img = target_img[:b//2, ...]
         
         # train mapping network
         # TODO: add mixing noise(for style mixing)
@@ -117,10 +135,13 @@ class LitSystem(LightningModule):
         # self.maunal_backward(ce_loss, opt_gm)
         # opt_gm.step()
         
+        dlatents = interpolated_latents \
+                   if interpolated_latents is not None \
+                   else latent
         # train D
         requires_grad(self.g, False)
         requires_grad(self.d, True)
-        fake_imgs_e, _ = self.g([interpolated_latents], skip_mapping=True) # real latent
+        fake_imgs_e, _ = self.g([dlatents], skip_mapping=True) # real latent
         fake_pred = self.d(fake_imgs_e)
         real_pred = self.d(target_img)
         d_loss = logistic_loss(real_pred, fake_pred)
@@ -129,54 +150,66 @@ class LitSystem(LightningModule):
         self.manual_backward(d_loss, opt_d)
         opt_d.step()
         
-        d_reg = batch_idx % self.d_reg_every == 0
-        if use_reg and d_reg:
+        if use_reg and batch_idx % self.d_reg_every == 0:
             target_img.requires_grad = True
             real_pred = self.d(target_img)
             r1_loss = d_r1_loss(real_pred, target_img)
-            r1_loss = self.r1 / 2 * r1_loss * self.d_reg_every + 0 * real_pred[0]
+            r1_loss = self.r1 / 2 * r1_loss * self.d_reg_every
             r1_loss_val = r1_loss.item()
             opt_d.zero_grad()
             self.manual_backward(r1_loss, opt_d)
             opt_d.step()
-            
+            self.logger.experiment.add_scalar('Metric/Dreg-R1', r1_loss_val, batch_idx)
         
         # train G
         requires_grad(self.g, True)
         requires_grad(self.d, False)
-        # real latent & interpolated latent
-        # fake_imgs_e, _ = self.g([latent], skip_mapping=True)
+        
         # reconstruction loss
+        # fake_imgs_e, _ = self.g([latent], skip_mapping=True)
         # fake_imgs_e = torch.sigmoid(fake_imgs_e)
-        # ssim_loss = - self.ssim_loss((target_img + 1) / 2., fake_imgs_e)
-        # ssim_val = - ssim_loss.detach()
+        # ssim_loss = -self.ssim_loss((target_img + 1) / 2., fake_imgs_e)
+        # ssim_val = -ssim_loss.detach()
         
-        fake_imgs_i, _ = self.g([interpolated_latents], skip_mapping=True)
+        # GAN
+        fake_imgs_i, _ = self.g([dlatents], skip_mapping=True)
         fake_pred_i = self.d(fake_imgs_i)
+        g_GANloss = nonsaturating_loss(fake_pred_i)
+        g_loss_val = g_GANloss.detach()
         
-        g_loss = nonsaturating_loss(fake_pred_i)
-        g_loss_val = g_loss.detach()
+        total_Gloss = g_GANloss# + 0.5 * ssim_loss
         opt_gs.zero_grad()
-        self.manual_backward(g_loss, opt_gs)
+        self.manual_backward(total_Gloss, opt_gs)
         opt_gs.step()
         
-        g_reg = batch_idx % self.g_reg_every == 0
-        if use_reg and g_reg:
-            # TODO: PPL on which latents(real or interpolated)
+        if use_reg and batch_idx % self.g_reg_every == 0:
+            dlatents.requires_grad = True
+            fake_imgs_i, dlatents = self.g([dlatents], skip_mapping=True, return_latents=True)
             path_loss, self.mean_path_length, self.path_lengths = path_regularize(
-                fake_imgs_i, interpolated_latents, self.mean_path_length
+                fake_imgs_i, dlatents, self.mean_path_length
             )
             weighted_path_loss = self.path_regularize * self.g_reg_every * path_loss
             weighted_path_loss_val = weighted_path_loss.item()
             opt_gs.zero_grad()
             self.manual_backward(weighted_path_loss, opt_gs)
             opt_gs.step()
-            
+            self.logger.experiment.add_scalar('Metric/Greg-PPL', weighted_path_loss_val, batch_idx)
+            self.logger.experiment.add_scalar('Metric/G-MeanPath', self.mean_path_length, batch_idx)
+            self.logger.experiment.add_scalar('Metric/G-PathLength', self.path_lengths, batch_idx)
 
         self.log('Metric/G_GANloss', g_loss_val, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log('Metric/D-GANloss', d_loss_val, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        #self.log('Metric/G-PPL-reg', weighted_path_loss_val, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        #self.log('Metric/D-r1-reg', r1_loss_val, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        
+        return total_Gloss + d_loss
+        # embed = self.decoder.embed.weight
+        # norms = torch.linalg.norm(embed, dim=1)
+        # embed_mean, embed_std = norms.mean().item(), norms.std().item()
+        # norms_l1 = torch.linalg.norm(self.decoder.linear1.weight).item()
+        # norms_l2 = torch.linalg.norm(self.decoder.linear2.weight).item()
+        # self.log('Stats/EmbedNorm-Mean', embed_mean, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        # self.log('Stats/EmbedNorm-Std', embed_std, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        # self.log('Stats/Linear1Norm', norms_l1, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        # self.log('Stats/Linear2Norm', norms_l2, on_step=True, on_epoch=True, prog_bar=True, logger=True)
     
     def training_epoch_end(self, training_step_outputs):
         if self.current_epoch == 0 or \
@@ -184,14 +217,42 @@ class LitSystem(LightningModule):
             self.log_sample_images()
             self.log_sample_images(mode='test')
             self.log_interpolated_images()
-    
-        # self.logger.log_hyperparams(
-        #     params=self.hparams,
-        #     metrics={
-        #         
-        #     }
-        # )
+            
+    def validation_step(self, batch, batch_idx):
+        latent, target_imgs = batch
+        with torch.no_grad():
+            fake_imgs, _ = self.g([latent], skip_mapping=True)
+            metrics = {
+                'mse': self.mse_loss(fake_imgs, target_imgs).item(),
+                'ssim': self.ssim_loss((target_imgs + 1) / 2., torch.sigmoid(fake_imgs)).item(),
+                'lpips': self.percept((target_imgs + 1) / 2., torch.sigmoid(fake_imgs)).mean().item()
+            }
+        # TODO: sync_dist=True
+        self.log('Metric/Val-MSE', metrics['mse'], on_epoch=True, prog_bar=True, logger=True)
+        self.log('Metric/Val-SSIM', metrics['ssim'], on_epoch=True, prog_bar=True, logger=True)
+        self.log('Metric/Val-LPIPS', metrics['lpips'], on_epoch=True, prog_bar=True, logger=True)
         
+        return metrics
+    
+    def validation_epoch_end(self, validation_step_outputs):
+        epoch_mse = mean([x['mse'] for x in validation_step_outputs])
+        epoch_ssim = mean([x['ssim'] for x in validation_step_outputs])
+        epoch_lpips = mean([x['lpips'] for x in validation_step_outputs])
+        
+        if epoch_mse < self.best_mse:
+            self.best_mse = epoch_mse
+        if epoch_ssim > self.best_ssim:
+            self.best_ssim = epoch_ssim
+        if epoch_lpips < self.best_lpips:
+            self.best_lpips = epoch_lpips
+        
+        self.logger.log_hyperparams(params=self.hparams,
+                                    metrics={
+                                        'val_MSE': self.best_mse,
+                                        'val_SSIM': self.best_ssim,
+                                        'val_LPIPS': self.best_lpips
+                                    })
+
     def configure_optimizers(self):
         g_reg_ratio = self.g_reg_every / (self.g_reg_every + 1)
         d_reg_ratio = self.d_reg_every / (self.d_reg_every + 1)
--- train.py
+++ train.py
@@ -1,140 +1,60 @@
-import pickle
 from pathlib import Path
 from argparse import ArgumentParser
-from random import sample, choice
-from functools import reduce
-
-import numpy as np
-import skimage.io as io
-import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-from tqdm import tqdm
-from PIL import Image
-from skimage.transform import resize
-from torch.utils import data
-from torch.utils.data import DataLoader
-from torchvision import transforms
-# from torch.autograd import Variable
-# from torch.utils.tensorboard import SummaryWriter
+from random import sample
 
 from pytorch_lightning import Trainer
 from pytorch_lightning import loggers as pl_loggers
 
 from models import LitSystem
-
-
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, latents, img_paths, transforms=None):
-        assert len(img_paths) == latents.shape[0]
-        self.latents = latents
-        self.img_paths = img_paths
-        self.transforms = transforms
-
-    def __len__(self):
-        return self.latents.shape[0]
-    
-    def __getitem__(self, index):
-        latent = self.latents[index]
-        target_img = io.imread(self.img_paths[index])
-        
-        if self.transforms:
-            target_img = self.transforms(target_img)
-            
-        return latent, target_img
-
-def get_dataloaders(root_dir, latent_path, target_dir, data_split,
-                    num_workers=1, latent_dim=512, bs_per_gpu=32):
-    root_dir = Path(root_dir).expanduser() \
-               if root_dir.startswith('~') \
-               else Path(root_dir)
-    img_list = sorted(list((root_dir / target_dir).glob('*/*.png')))
-
-    latents = []
-    if Path(latent_path).is_dir():
-        latent_paths = sorted(list((root_dir / latent_path).glob("*.pkl")))
-        for p in tqdm(latent_paths):
-            latents.append(pickle.loads(p.read_bytes())[:, :latent_dim])
-        latents = torch.from_numpy(np.concatenate(latents, axis=0))
-    elif Path(latent_path).suffix == ".npy":
-        latents = torch.from_numpy(np.load(root_dir / latent_path)[:, :latent_dim])
-    else:
-        raise NotImplementedError("feat format not supported")
-
-    if latents.dtype == torch.float64:
-        latents = latents.float()
-    assert len(img_list) == latents.shape[0], f"#latent & #img are not match {latents.shape[0]} v.s. {len(img_list)}"
-    assert sum(data_split) <= len(img_list)
-    
-    trf = [
-        transforms.ToTensor(),
-        transforms.Normalize([0.5]*3, [0.5]*3, inplace=True),
-    ]
-    trfs = transforms.Compose(trf)
-     
-    idx = 0
-    dataloaders = []   
-    split_names = ['train', 'val', 'test']
-    for split, n_records in zip(split_names, data_split):
-        l = latents[idx:idx+n_records]
-        img_paths = img_list[idx:idx+n_records]
-        idx += n_records
-        
-        dataset = Dataset(l, img_paths, transforms=trfs)
-        dataloaders.append(
-            DataLoader(
-                dataset,
-                bs_per_gpu,
-                num_workers=num_workers,
-                shuffle=(split == 'train'),
-                drop_last=True
-            )
-        )
-    
-    return dataloaders
+from dataset import get_dataloaders
 
 
 def main(args):
+    assert args.num_sample <= args.train_size and args.num_sample <= args.test_size
+    
     data_split = [args.train_size, args.val_size, args.test_size]
     
-    train_loader, val_loader, test_loader = \
+    (train_loader, val_loader, test_loader), train_latents = \
         get_dataloaders(args.root_dir, args.latent_path, args.target_dir, data_split,
                         args.num_workers, args.latent_dim, args.bs_per_gpu)
     
     samples = {}
-    latent, target_img = next(iter(train_loader))
+    indices, latents, target_imgs = next(iter(train_loader))
+    
     samples['train'] = {
-        'latents': latent[:args.num_sample],
-        'targets': target_img[:args.num_sample]
+        'indices': indices[:args.num_sample],
+        'latents': latents[:args.num_sample],
+        'targets': target_imgs[:args.num_sample]
     }
-    latent, target_img = next(iter(test_loader))
+    latents, target_imgs = next(iter(test_loader))
     samples['test'] = {
-        'latents': latent[:args.num_sample],
-        'targets': target_img[:args.num_sample]
+        'latents': latents[:args.num_sample],
+        'targets': target_imgs[:args.num_sample]
     }
 
-    train_system = LitSystem(args.log_sample_every,
-                             samples,
+    train_system = LitSystem(samples,
+                             args.log_sample_every,
                              lr=args.lr,
                              batch_size=(args.n_gpu*args.bs_per_gpu),
                              latent_dim=args.latent_dim,
-                             train_size=args.train_size)
-    tb_logger = pl_loggers.TensorBoardLogger('runs', name=args.log_name, version=args.version)
+                             train_size=args.train_size,
+                             val_size=args.val_size)
+
+    tb_logger = pl_loggers.TensorBoardLogger(
+        'runs', name=args.log_name, version=args.version, default_hp_metric=False)
+    
+    # maybe load from checkpoints
     kwargs = dict(logger=tb_logger, distributed_backend='ddp', automatic_optimization=False)
     if not args.resume_from_checkpoint:
-        kwargs.update({
-            'resume_from_checkpoint': args.resume_from_checkpoint
-        })
+        kwargs['resume_from_checkpoint'] = args.resume_from_checkpoint
     
     trainer = Trainer.from_argparse_args(args, **kwargs)
-    trainer.fit(train_system, train_loader)
+    trainer.fit(train_system, train_loader, val_loader)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    
-#     parser.add_argument('--max_steps', type=int, default=1)
+
+    # general
     parser.add_argument('--num_sample', type=int, default=32)
     parser.add_argument('--log_name', type=str, default='default')
     parser.add_argument('--version', type=str, default=None)
@@ -142,18 +62,18 @@ if __name__ == '__main__':
     # data
     parser.add_argument('--root_dir', type=str, default='~/data/FFHQ')
     parser.add_argument('--latent_path', type=str, default='feat_PCA_L5_1024')
-    parser.add_argument('--target_dir', type=str, default='images256x256')
-
+    parser.add_argument('--target_dir', type=str, \
+                        default='images256x256', help="path to target(image) directory")
     parser.add_argument('--num_workers', type=int, default=3)
     parser.add_argument('--train_size', type=int, default=3200)
     parser.add_argument('--val_size', type=int, default=1000)
     parser.add_argument('--test_size', type=int, default=1000)
+    
+    # model
     parser.add_argument('--bs_per_gpu', type=int, default=32)
     parser.add_argument('--latent_dim', type=int, default=512)
     
     parser = LitSystem.add_model_specific_args(parser)
-    
-    # add trainer args
     parser = Trainer.add_argparse_args(parser)
     # gpus, max_epochs, (max_steps),
     # resume_from_checkpoints, sync_batchnorm
@@ -161,7 +81,6 @@ if __name__ == '__main__':
     # distributed_backend
 
     args = parser.parse_args()
-
     try:
         args.gpus = int(args.gpus)
         args.n_gpu = args.gpus
