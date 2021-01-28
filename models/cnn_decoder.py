5e0f52b3429a5d52e6c1a152e100b5661779f9b0
jimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2021-01-28 20:07:22 +0800

    replace layernorm to SN on 2 linear layer

--- models/cnn_decoder.py
+++ models/cnn_decoder.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torch.nn.utils import spectral_norm
 
 class CNNDecoder(nn.Module):
     def __init__(self,
@@ -21,10 +22,8 @@ class CNNDecoder(nn.Module):
         
         n_ch = [256, 256, 128, 128, 64, 64, 3]
         conv_blocks = []
-        self.linear1 = nn.Linear(latent_dim, 4096)
-        self.linear1_In = nn.LayerNorm(4096, elementwise_affine=False)
-        self.linear2 = nn.Linear(4096, 8*8*256)
-        self.linear2_In = nn.LayerNorm(8*8*256, elementwise_affine=False)
+        self.linear1 = spectral_norm(nn.Linear(latent_dim, 4096))
+        self.linear2 = spectral_norm(nn.Linear(4096, 8*8*256))
         self.act = nn.LeakyReLU(0.2)
         print(f"using norm: {norm_type}")
 
@@ -36,7 +35,7 @@ class CNNDecoder(nn.Module):
                 conv = nn.ConvTranspose2d(n_ch[i],n_ch[i+1], kernel_size=4, stride=2, padding=1)
                 
             if norm_type == 'spectral_norm' and i in [0,1,2,3,4,5]:
-                conv_sn = nn.utils.spectral_norm(conv)
+                conv_sn = spectral_norm(conv)
                 conv_blocks.append(conv_sn)
             elif norm_type == 'batch_norm' and i in [4,5]:
                 bn = nn.BatchNorm2d(n_ch[i+1])
@@ -61,10 +60,9 @@ class CNNDecoder(nn.Module):
             return latent
         
         x = self.linear1(latent)
-        x = self.linear1_In(x)
         x = self.act(x)
         x = self.linear2(x)
-        x = self.linear2_In(x).view(-1,256,8,8)
+        x = x.view(-1,256,8,8)
         
         for layer in self.conv_blocks:
             x = layer(x)
